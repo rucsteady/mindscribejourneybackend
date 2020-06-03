6b8e8000359a370c2a4c00893238c2b490106dca
rucsteady <rucsteady@gmx.de>
2020-06-03 11:20:12 +0200

    added first model

--- main.js
+++ main.js
@@ -2,17 +2,33 @@ const express = require("express"),
   app = express(),
   homeController = require("./controllers/homeController"),
   errorController = require("./controllers/errorController"),
+  mongoose = require("mongoose"),
+  Liker = require("./models/like"),
   layouts = require("express-ejs-layouts");
+
+mongoose.connect("mongodb://localhost:27017/todaydb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("Succesfully connected to MongoDB using Mongoose!");
+});
+
+var myQuery = Liker.findOne({
+  name: "Nils",
+}).where("message", /bike/);
+myQuery.exec((error, data) => {
+  if (data) console.log(data.name);
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use(layouts);
 app.use(express.static("public"));
-
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
-
 app.get("/", homeController.getIndex);
 app.get("/likes", homeController.showLikes);
 app.get("/shirts", homeController.getShirts);
---
+++ models/like.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const likeSchema = mongoose.Schema({
+  name: String,
+  message: String,
+});
+
+module.exports= mongoose.model("Liker", likeSchema);
+
