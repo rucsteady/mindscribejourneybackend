23000be27d9aa0f19070efb8af2ec9a69ae27239
rucsteady <rucsteady@gmx.de>
2020-06-03 09:07:10 +0200

    added error handles

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express"),
   app = express(),
   homeController = require("./controllers/homeController"),
+  errorController = require("./controllers/errorController"),
   layouts = require("express-ejs-layouts");
 
 app.use(express.urlencoded({ extended: false }));
@@ -8,6 +9,7 @@ app.use(express.json());
 
 app.use(layouts);
 app.use(express.static("public"));
+
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
@@ -18,6 +20,9 @@ app.get("/shirts", homeController.getShirts);
 app.get("/contact", homeController.showSignUp);
 app.post("/contact", homeController.postedSignUpForm);
 app.post("/", homeController.postedLikeUpForm);
+app.use(errorController.respondInternalError);
+app.use(errorController.respondNoResourceFound);
+
 
 app.listen(app.get("port"), () => {
   console.log(`Server running at port: ${app.get("port")}`);
