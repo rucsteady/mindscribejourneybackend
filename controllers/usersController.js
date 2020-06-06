4c001177f989172e1cc513f7fe7c684e4b4dc77a
rucsteady <rucsteady@gmx.de>
2020-06-06 11:42:31 +0200

    ser create and post rediret works!

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const User = require("../models/user");
 module.exports = {
   index: (req, res, next) => {
@@ -12,6 +14,35 @@ module.exports = {
       });
   },
   indexView: (req, res) => {
-    res.render("users/index");
+    res.render("users");
+  },
+  new: (req, res) => {
+    res.render("users/new");
+  },
+  create: (req, res, next) => {
+    let userParams = {
+      name: {
+        first: req.body.first,
+        last: req.body.last,
+      },
+      email: req.body.email,
+      password: req.body.password,
+      zipCode: req.body.zipCode,
+    };
+    User.create(userParams)
+      .then((user) => {
+        res.locals.redirect = "/users";
+        res.locals.user = user;
+        next();
+      })
+      .catch((error) => {
+        console.log(`Error saving user: ${error.message}`);
+        next(error);
+      });
+  },
+  redirectView: (req, res, next) => {
+    let redirectPath = res.locals.redirect;
+    if (redirectPath) res.redirect(redirectPath);
+    else next();
   },
 };
--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const express = require("express"),
   likersController = require("./controllers/likersController"),
   subscribersController = require("./controllers/subscribersController"),
   usersController = require("./controllers/usersController"),
+  router = express.Router(),
   User = require("./models/user"),
   layouts = require("express-ejs-layouts");
 
@@ -24,32 +25,31 @@ db.once("open", () => {
   console.log("Succesfully connected to MongoDB using Mongoose!");
 });
 
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(layouts);
 app.use(express.static("public"));
+app.use(layouts);
+router.use(layouts);
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
 
+app.use("/", router);
+router.get("/users", usersController.index, usersController.indexView);
+router.get("/users/new", usersController.new);
+router.post("/users/create", usersController.create, usersController.redirectView);
+
+
 app.get("/", homeController.getIndex);
 app.post("/", likersController.saveLiker);
-
-/*
-Liker.deleteMany()
-.exec()
-.then(() => {
-  console.log("Likers database is now empty");
-});
-*/
-
 app.get("/likes", likersController.getAllLikers);
 app.get("/shirts", homeController.getShirts);
 app.get("/contact", homeController.showSignUp);
 app.get("/subscribers", subscribersController.getAllSubscribers);
 app.post("/contact", subscribersController.saveSubscriber);
 
-app.get("/users", usersController.index, usersController.indexView);
+
 
 app.use(errorController.respondInternalError);
 app.use(errorController.respondNoResourceFound);
