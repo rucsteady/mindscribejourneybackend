ae2bd0c57ba79bcbe46d3e7c0e8642c6b28f701f
rucsteady <rucsteady@gmx.de>
2020-06-07 11:37:12 +0200

    added update user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -49,16 +49,53 @@ module.exports = {
   show: (req, res, next) => {
     let userId = req.params.id;
     User.findById(userId)
-      .then(user => {
+      .then((user) => {
         res.locals.user = user;
         next();
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(`Error fetching user by ID: ${error.message}`);
         next(error);
       });
   },
   showView: (req, res) => {
     res.render("users/show");
-  }
+  },
+  edit: (req, res, next) => {
+    let userId = req.params.id;
+    User.findById(userId)
+      .then((user) => {
+        res.render("users/edit", {
+          user: user,
+        });
+      })
+      .catch((error) => {
+        console.log(`Error fetching user by ID: ${error.message}`);
+        next(error);
+      });
+  },
+  update: (req, res, next) => {
+    let userId = req.params.id,
+      userParams = {
+        name: {
+          first: req.body.first,
+          last: req.body.last,
+        },
+        email: req.body.email,
+        password: req.body.password,
+        zipCode: req.body.zipCode,
+      };
+    User.findByIdAndUpdate(userId, {
+      $set: userParams,
+    })
+      .then((user) => {
+        res.locals.redirect = `/users/${userId}`;
+        res.locals.user = user;
+        next();
+      })
+      .catch((error) => {
+        console.log(`Error updating user by ID: ${error.message}`);
+        next(error);
+      });
+  },
 };
--- main.js
+++ main.js
@@ -13,29 +13,35 @@ const express = require("express"),
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-  "mongodb://nils12:nils12@ds157707.mlab.com:57707/heroku_1bw65rfv", {
+    "mongodb://nils12:nils12@ds157707.mlab.com:57707/heroku_1bw65rfv",
+  {
     useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true,
   }
 );
 const db = mongoose.connection;
 mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', false);
 
 db.once("open", () => {
   console.log("Succesfully connected to MongoDB using Mongoose!");
 });
 
-
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(express.json());
 app.use(express.static("public"));
 app.use(layouts);
 router.use(layouts);
-router.use(methodOverride("_method", {
-  methods: ["POST", "GET"]
-}));
+router.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
@@ -43,8 +49,15 @@ app.set("view engine", "ejs");
 app.use("/", router);
 router.get("/users", usersController.index, usersController.indexView);
 router.get("/users/new", usersController.new);
-router.post("/users/create", usersController.create, usersController.redirectView);
+router.post(
+  "/users/create",
+  usersController.create,
+  usersController.redirectView
+);
 router.get("/users/:id", usersController.show, usersController.showView);
+router.get("/users/:id/edit", usersController.edit);
+router.put("/users/:id/update", usersController.update, usersController.redirectView);
+
 
 app.get("/", homeController.getIndex);
 app.post("/", likersController.saveLiker);
@@ -53,8 +66,6 @@ app.get("/shirts", homeController.getShirts);
 app.get("/contact", homeController.showSignUp);
 app.get("/subscribers", subscribersController.getAllSubscribers);
 app.post("/contact", subscribersController.saveSubscriber);
-
-
 
 app.use(errorController.respondInternalError);
 app.use(errorController.respondNoResourceFound);
