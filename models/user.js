4d07e3469ba532c7ca809759a3d55a7be32e22f2
rucsteady <rucsteady@gmx.de>
2020-06-14 19:57:06 +0200

    added bcrypt hast user pw

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const Subscriber = require("./subscriber");
+const bcrypt = require("bcrypt");
 const mongoose = require("mongoose"),
   { Schema } = mongoose,
   userSchema = new Schema(
@@ -67,5 +68,22 @@ userSchema.pre("save", function (next) {
     next();
   }
 });
+userSchema.pre("save", function (next) {
+  let user = this;
+  bcrypt
+    .hash(user.password, 10)
+    .then((hash) => {
+      user.password = hash;
+      next();
+    })
+    .catch((error) => {
+      console.log(`Error in hashing password: ${error.message}`);
+      next(error);
+    });
+});
+userSchema.methods.passwordComparison = function (inputPassword) {
+  let user = this;
+  return bcrypt.compare(inputPassword, user.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
