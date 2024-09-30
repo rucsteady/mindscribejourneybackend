9c8f90c4b0b74a5bf02459abc4185b5a7b073076
Nils Rausch <nils.rausch@exxeta.com>
2024-09-30 17:10:53 +0200

    likesRoutes

--- routes/likeRoutes.js
+++ routes/likeRoutes.js
@@ -4,15 +4,11 @@ import * as likesController from "../controllers/likesController.js";
 const router = express.Router();
 
 router.get("/", likesController.index);
-
 router.post("/create", likesController.create);
-
 router.get("/:id", likesController.show);
-
 router.put("/:id/update", likesController.update);
-
 router.delete("/:id/delete", likesController.deleteLike);
-
 router.post("/delete/Likes", likesController.deleteLikes);
+router.post("/:id/join", likesController.join);
 
 export default router;
